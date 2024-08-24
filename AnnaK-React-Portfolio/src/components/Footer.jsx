820d06f181e6dc5d29441d88b6194140d90dc6af
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-08-24 11:16:55 -0700

    create Footer.jsx component, add css style for footer and css variables to the App.css

---
+++ AnnaK-React-Portfolio/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="w-100 mt-auto text-dark p-4">
+            <div className="container text-center mb-5">
+                <ul>
+                    <li><a href="https://github.com/explorer7733">GitHub</a></li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
