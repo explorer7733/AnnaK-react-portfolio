b63e1818ae6c97d3f88f06ed302f4b3447cb7513
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-08-24 11:46:05 -0700

    create Aboutme.jsx and Error.jsx pages

---
+++ AnnaK-React-Portfolio/src/pages/Aboutme.jsx
@@ -0,0 +1,15 @@
+
+import personalPhoto from '../assets/personal-photo.jpg';
+export default function Aboutme() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <div>
+                <img src={personalPhoto} alt="personal photo" className="photo">
+            </div>
+            <p>
+                I am going to become a web developer. I always try to go out of my comfort zone and learn something new. My interests are yoga, taichi, paddle boarding, street photography.
+            </p>
+        </div>
+    );
+}
