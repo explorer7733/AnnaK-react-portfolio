67902252d3b3c1b0d2aee42a84a81449ce008e68
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-08-30 15:12:30 -0700

    modify Resume.jsx component and its style at App.css

--- AnnaK-React-Portfolio/src/pages/Resume.jsx
+++ AnnaK-React-Portfolio/src/pages/Resume.jsx
@@ -3,25 +3,45 @@ function Resume() {
         <div className="resume">
             <h1 className="titleResume">Resume</h1>
             <p>Download my resume</p>
-            <div className="skills">
-                <h2 className="headerSkills">Front-end skills:</h2>
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>Bootstrap</li>
-                </ul>
-            </div>
-            <div className="skills">
-                <h3 className="headerSkills">Back-end skills:</h3>
-                <ul>
-                    <li>Node.js</li>
-                    <li>Express</li>
-                    <li>MySQL</li>
-                    <li>MongoDB</li>
-                    <li>Sequelize</li>
-                </ul>
+            <div className="skills-container">
+                <div className="skills front-end">
+                    <h2 className="headerSkills">Front-end skills:</h2>
+                    <ul>
+                        <li>HTML/CSS</li>
+                        <li>CSS Frameworks</li>
+                        <li>JavaScript/JQuery</li>
+                        <li>React</li>
+                        <li>Responsive Design</li>
+                        <li>DOM Manipulation</li>
+                        <li>MySQL</li>
+                    </ul>
+                </div>
+                <div className="divider"></div>
+                <div className="skills back-end">
+                    <h3 className="headerSkills">Back-end skills:</h3>
+                    <ul>
+                        <li>Node.js</li>
+                        <li>Express.js</li>
+                        <li>GraphQL</li>
+                        <li>MongoDB/PostgreSQL</li>
+                        <li>Sequelize</li>
+                        <li>REST/JSON</li>
+                        <li>Git/Github</li>
+                    </ul>
+                </div>
+                <div className="divider"></div>
+                <div className="skills soft-skills">
+                    <h3 className="headerSkills">Soft skills:</h3>
+                    <ul>
+                        <li>Creativity</li>
+                        <li>Communication</li>
+                        <li>Teamwork</li>
+                        <li>Growth mindset</li>
+                        <li>Problem-solving</li>
+                        <li>Time Management</li>
+                        <li>Situational Awareness</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
