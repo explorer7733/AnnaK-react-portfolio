e2fb7af4349070177728d05943abe027b9309e23
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-09-01 10:34:24 -0700

    modify Aboutme component and css at App.css

--- AnnaK-React-Portfolio/src/pages/Aboutme.jsx
+++ AnnaK-React-Portfolio/src/pages/Aboutme.jsx
@@ -1,14 +1,33 @@
 import personalPhoto from '../assets/personal-photo.jpg'
- function Aboutme() {
+function Aboutme() {
     return (
         <div className="aboutme">
             <h1 className="titleAboutme">About Me</h1>
             <div className="aboutme-content">
                 <img src={personalPhoto} alt="personal photo" className="photo" />
-            <p>
-                I am going to become a web developer. I always try to go out of my comfort zone and learn something new. 
-                My interests are yoga, taichi, paddle boarding, street photography.
-            </p>
+                <div className="greeting">
+                    <h2 className="greetings">Hello, my name is <br><span className="name">Anna</span></br> and I'm a UI/UX designer/front-end developer with a passion for the web design.</h2>
+                </div>
+                <p className="statement">
+                    <span>There </span>
+                    <span>are </span>
+                    <span>no </span>
+                    <span>limits </span>
+                    <span>to </span>
+                    <span>what </span>
+                    <span>you </span>
+                    <span>can </span>
+                    <span>accomplish, </span>
+                    <span>except </span>
+                    <span>the </span>
+                    <span>limits </span>
+                    <span>you </span>
+                    <span>place </span>
+                    <span>on </span>
+                    <span>your </span>
+                    <span>own </span>
+                    <span>thinking.</span>
+                </p>
             </div>
         </div>
     );
